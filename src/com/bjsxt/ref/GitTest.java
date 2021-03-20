0999cae1d5babf9c3aa17d8c25dde8ec33ee1ab0
chen <chen1443683598@outlook.com>
2021-03-20 20:34:44 +0800

    第2次新增GitTest

--- src/com/bjsxt/ref/GitTest.java
+++ src/com/bjsxt/ref/GitTest.java
@@ -3,5 +3,6 @@ package com.bjsxt.ref;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一次新增GitTest");
+        System.out.println("第2次新增GitTest");
     }
 }
